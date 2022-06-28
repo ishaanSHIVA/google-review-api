d80081d653aaafc86ced2f6bb5227e92a052ebbf
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-28 23:31:13 +0530

    complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    GOOGLE_MAPS_PLACE_ID: process.env.GOOGLE_MAPS_PLACE_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
